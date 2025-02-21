0129a9aba634cf8b8c6a9fcdc1485a785117d459
rosar <rosario.amantia@outlook.it>
2025-02-21 11:57:18 +0100

    added backend service init

---
+++ boatly-server/src/main/java/com/rosario/boatly/boatly_server/BoatlyServerApplication.java
@@ -0,0 +1,13 @@
+package com.rosario.boatly.boatly_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatlyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatlyServerApplication.class, args);
+	}
+
+}
---
+++ boatly-server/src/test/java/com/rosario/boatly/boatly_server/BoatlyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosario.boatly.boatly_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatlyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
